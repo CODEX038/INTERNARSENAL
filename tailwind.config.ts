276b5198ffc923296a7539da07efd023bf43f996
salvishreepad-prog <shreepadsalvi@gmail.com>
2026-05-29 18:02:08 +0530

    fix: tailwind config type using satisfies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
   darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -44,6 +44,6 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+} satisfies Config
 
 export default config
